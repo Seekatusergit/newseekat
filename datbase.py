8255ad5b1eea652d929f3061e7e26f9174a4deba
juanjesus260393 <89158157+jugarcen2021@users.noreply.github.com>
2022-05-25 20:51:07 -0500

    ejemplo base de datos v1

--- datbase.py
+++ datbase.py
@@ -1,33 +1,17 @@
-import os
-import dash
-from dash import dcc
-from dash import html
-import pandas as pd
-import plotly.graph_objs as go
-import cdata.mysql as mod
+import mysql.connector
 
-cnxn = mod.connect("User=root;Password=;Database=test;Server=127.0.0.1;Port=3306;")
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="",
+  database="test"
+)
 
-df = pd.read_sql("SELECT * FROM test", cnxn)
+mycursor = mydb.cursor()
 
-app_name = 'dash-mysqledataplot'
- 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
- 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = 'CData + Dash'
+mycursor.execute("SELECT * FROM test")
 
-trace = go.Bar(x=df.ShipName, y=df.Freight, name='ShipName')
- 
-app.layout = html.Div(children=[html.H1("CData Extension + Dash", style={'textAlign': 'center'}),
-dcc.Graph(
-id='example-graph',
-figure={
-'data': [trace],
-'layout':
-go.Layout(title='MySQL Orders Data', barmode='stack')
-})
-], className="container")
+myresult = mycursor.fetchall()
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+for x in myresult:
+  print(x)
